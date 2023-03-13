c4c23da93913cf694e364967f9f553cfdc10866f
heoseongeun <82304095+heoseongeun@users.noreply.github.com>
2023-03-13 19:21:22 +0900

    [level 2] Title: 소수 찾기, Time: 3.24 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -26,7 +26,7 @@ class Solution {
     
     private static boolean isPrime(int num) {
         if(num <= 1) return false;
-        for(int i = 2; i < num / 2 + 1; i++) {
+        for(int i = 3; i < Math.sqrt(num); i =+ 2) {
             if(num % i == 0) return false;
         }
         return true;
