f6ddf8eab0a63ad072b953aadc5565c811c27570
heoseongeun <82304095+heoseongeun@users.noreply.github.com>
2023-05-17 20:13:39 +0900

    [Silver I] Title: 동물원, Time: 92 ms, Memory: 15264 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        long[][] dp = new long[N + 1][3];
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n + 1][3];
         dp[1][0] = dp[1][1] = dp[1][2] = 1;
-        for(int i = 2; i <= N; i++) {
+        for(int i = 2; i <= n; i++) {
             dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+            dp[i][1] = (dp[i -1][0] + dp[i - 1][2]) % 9901;
             dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
         }
-        bw.write(((dp[N][0] + dp[N][1] + dp[N][2]) % 9901) + "");
+        bw.write("" + (dp[n][0] + dp[n][1] + dp[n][2]) % 9901);
         bw.flush();
         bw.close();
         br.close();
