27cc4b75356eb4c7e605adba34e1991de931302c
heoseongeun <82304095+heoseongeun@users.noreply.github.com>
2023-05-31 18:46:09 +0900

    [level 2] Title: 구명보트, Time: 8.96 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int index = 0;
+		for (int i = people.length - 1; i >= 0; i--) {
+			if (index > i) break;
+			if (people[i] + people[index] <= limit) index++;
+			answer++;
+		}
+        return answer;
+    }
+}
