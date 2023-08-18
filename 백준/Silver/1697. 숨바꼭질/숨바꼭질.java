46f66d8814db39e71c5bb68e8f963cd3532d93ce
heoseongeun <82304095+heoseongeun@users.noreply.github.com>
2023-08-18 17:30:44 +0900

    [Silver I] Title: 숨바꼭질, Time: 120 ms, Memory: 16200 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, K;
+    static int[] visited = new int[100001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        bw.write(bfs(N) + "");
+        bw.close();
+        br.close();
+    }
+
+    static int bfs(int num) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(num);
+        visited[num] = 1;
+        while(!q.isEmpty()) {
+            int n = q.poll();
+            if(n == K) return visited[n] - 1;
+            if(n - 1 >= 0 && visited[n - 1] == 0) {
+                visited[n - 1] = visited[n] + 1;
+                q.add(n - 1);
+            }
+            if(n + 1 <= 100000 && visited[n + 1] == 0) {
+                visited[n + 1] = visited[n] + 1;
+                q.add(n + 1);
+            }
+            if(2 * n <= 100000 && visited[2 * n] == 0) {
+                visited[2 * n] = visited[n] + 1;
+                q.add(2 * n);
+            }
+        }
+        return -1;
+    }
+}
