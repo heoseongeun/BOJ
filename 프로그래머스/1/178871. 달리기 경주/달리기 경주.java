ac4a4a2b8c8dac18bcbd7dbace9eaab61e4db9fc
heoseongeun <82304095+heoseongeun@users.noreply.github.com>
2023-11-10 21:50:58 +0900

    [level 1] Title: 달리기 경주, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for(int i = 0; i < players.length; i++) map.put(players[i], i);
+        for(int i = 0; i < callings.length; i++){
+            int index = map.get(callings[i]);
+            players[index] = players[index - 1];
+            players[index - 1] = callings[i];
+            map.put(players[index], index);
+            map.put(players[index - 1], index - 1);
+        }
+        return players;
+    }
+}
