f456937ac6a2792bdd9c7f7418c62ca03d9628fe
heoseongeun <82304095+heoseongeun@users.noreply.github.com>
2024-03-21 21:16:42 +0900

    [level 2] Title: 호텔 대실, Time: 34.54 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    static class Booking {
+        private int start;
+        private int end;
+        public Booking(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }    
+    public int solution(String[][] book_time) {
+        PriorityQueue<Booking> pq = new PriorityQueue<>((o1, o2) -> (o1.start - o2.start));
+        for(int i = 0; i < book_time.length; i++) {
+            int start_h = Integer.parseInt(book_time[i][0].split(":")[0]);
+            int start_m = Integer.parseInt(book_time[i][0].split(":")[1]);
+            int end_h = Integer.parseInt(book_time[i][1].split(":")[0]);
+            int end_m = Integer.parseInt(book_time[i][1].split(":")[1]);
+            int start = (start_h * 60) + start_m;
+            int end = (end_h * 60) + end_m;
+            pq.add(new Booking(start, end));
+        }
+        List<Integer> end_times = new ArrayList<>();
+        while(!pq.isEmpty()) {
+            Booking b = pq.poll();
+            int start = b.start, end = b.end; 
+            boolean available = false;
+            Collections.sort(end_times);
+            for(int i = 0; i < end_times.size(); i++) {
+                if(end_times.get(i) + 10 <= start) {
+                    available = true;
+                    end_times.set(i, end);
+                    break;
+                }
+            }
+            if(!available) end_times.add(end);
+        }
+        return end_times.size();
+    }
+}
