d06b334dca9a90ba38843c5b8c1a7bf83be65105
heoseongeun <82304095+heoseongeun@users.noreply.github.com>
2024-05-02 11:28:57 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.16 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] lines) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] line : lines) {
+            for (int i = line[0]; i < line[1]; i++) 
+                map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        int cnt = 0;
+        for (int key : map.keySet()) {
+            if (map.get(key) > 1) cnt++;
+        }
+        return cnt;
+    }
+}
