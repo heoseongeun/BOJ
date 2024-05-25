8f829fa30d479e0c883bacb4fe639692b48b5ef4
heoseongeun <82304095+heoseongeun@users.noreply.github.com>
2024-05-25 20:03:43 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 228 ms, Memory: 14848 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -1,68 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    private int N, K;
-    private int[] belt;
-    private boolean[] robot;
-    private int res;
-
-    public void solution() throws IOException {
+    static int[] arr;
+    static boolean[] robot;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        belt = new int[N * 2];
-        robot = new boolean[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N * 2; i++) {
-            belt[i] = Integer.parseInt(st.nextToken());
-        }
-
-        res = 0;
-        while (K > 0) {
-            int temp = belt[N * 2 - 1];
-            for (int i = N * 2 - 1; i > 0; i--) {
-                belt[i] = belt[i - 1];
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        arr = new int[2 * n];
+        robot = new boolean[n];
+        for(int i = 0; i < 2 * n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        int step = 0;
+        while(0 < k) {
+            // 벨트와 함께 로봇 회전
+            int tmp = arr[arr.length - 1];
+            int idx = n - 1;
+            for(int i = arr.length - 1; i > 0; i--) {
+                arr[i] = arr[i - 1];
+                if(idx > 0) {
+                    robot[idx] = robot[idx - 1];
+                    idx--;
+                }
             }
-            belt[0] = temp;
-
-            for (int i = N - 1; i > 0; i--) {
-                robot[i] = robot[i - 1];
-            }
-            robot[N - 1] = false;
+            arr[0] = tmp;
             robot[0] = false;
-
-            for (int i = N - 1; i > 0; i--) {
-                if (!robot[i] && robot[i - 1] && belt[i] > 0) {
-                    belt[i]--;
-                    if (belt[i] == 0) {
-                        K--;
-                    }
-
+            robot[n - 1] = false;
+            // 로봇 회전
+            for(int i = n - 1; i > 0; i--) {
+                if(robot[i - 1] && !robot[i] && arr[i] >= 1) {
+                    arr[i]--;
+                    if(arr[i] == 0) k--;
                     robot[i] = true;
                     robot[i - 1] = false;
                 }
             }
-
-            if (belt[0] > 0) {
-                belt[0]--;
-                if (belt[0] == 0) {
-                    K--;
-                }
+            if(arr[0] > 0) {
                 robot[0] = true;
+                arr[0]--;
+                if(arr[0] == 0) k--;
             }
-            res++;
+            step++;
         }
-
-        System.out.println(res);
-    }
-
-    public static void main(String[] args) throws IOException {
-        new Main().solution();
+        bw.write(step + "");
+        bw.flush();
     }
 }
