d0169e2a12becaa9c7b17603e2d68e639191b92d
heoseongeun <82304095+heoseongeun@users.noreply.github.com>
2024-06-15 11:20:20 +0900

    [Silver III] Title: 주유소, Time: 380 ms, Memory: 40720 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -16,21 +16,13 @@ public class Main {
         for(int i = 0; i < n; i++) {
             price[i] = Integer.parseInt(st.nextToken());
         }
-        if(n == 2) bw.write(dist[0] * price[0] + "");
-        else if (n == 3) {
-            if(price[0] * (dist[0] + dist[1]) > price[0] * dist[0] + price[1] * dist[1])
-                bw.write((price[0] * dist[0] + price[1] * dist[1]) + "");
-            else bw.write((price[0] * (dist[0] + dist[1])) + "");
-        } else {
-            int[] dp = new int[n + 1];
-            dp[1] = dist[0] * price[0];
-            for(int i = 2; i < n; i++) {
-                dp[i] = Math.min(dp[i - 1] + (price[i - 1] * dist[i - 1]),
-                Math.min(dp[i - 2] + (price[i - 2] * (dist[i - 1] + dist[i - 2])),
-                dp[i - 2] + ((price[i - 2] * dist[i - 2]) + (price[i - 1] * dist[i - 1]))));
-            }
-            bw.write(dp[n - 1] + "");
+        long sum = 0;
+        long min = price[0];
+        for(int i = 0; i < n - 1; i++) {
+            if(price[i] < min) min = price[i];
+            sum += (min * dist[i]);
         }
+        bw.write(sum + "");
         bw.flush();
     }
 }
